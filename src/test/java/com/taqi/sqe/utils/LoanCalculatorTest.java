5a137fa76363fa91408728347cbe851cb269d07a
taqi-m <maliktaqikhokhar786@gmail.com>
2025-11-04 22:19:40 +0500

    Add LoanCalculator class Tests

--- src/test/java/com/taqi/sqe/utils/LoanCalculatorTest.java
+++ src/test/java/com/taqi/sqe/utils/LoanCalculatorTest.java
@@ -1,14 +1,44 @@
 package com.taqi.sqe.utils;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@Tag("slow")
 class LoanCalculatorTest {
 
+    private LoanCalculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new LoanCalculator();
+    }
+    
     @Test
-    void setupCheckTest() {
-        int expected = 2;
-        int actual = 1 + 1;
-        assertEquals(expected, actual, "Simple addition check");
+    @Order(1)
+    @DisplayName("Test 9: High Rate and Long Term")
+    void testHighRateLoan() {
+        double payment = calculator.calculateMonthlyPayment(500000, 12.5, 30);
+        assertEquals(5336.29, payment, "High rate, long term loan");
+    }
+
+    
+    //5 test cases from the CSV file.
+    @ParameterizedTest(name = "Test {index}: P={0}, R={1}%, Y={2} -> Expected {3}")
+    @CsvFileSource(resources = "/loanData.csv", numLinesToSkip = 1)
+    @Tag("integration")
+    @Order(2)
+    @DisplayName("Test 10-14: Calculate Correctly from CSV File")
+    void testLoanCalculationsFromCSV(double principal, double rate, int years, double expectedPayment) {
+        double actualPayment = calculator.calculateMonthlyPayment(principal, rate, years);
+        assertEquals(expectedPayment, actualPayment);
     }
 }
